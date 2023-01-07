592147d66986dacf90a8f645eb1f6bad22811703
Julia Ganina <ganina.julia2@gmail.com>
2023-01-07 17:38:01 +0000

    iterations 1-4 done Sirah and Julie

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,35 @@
+const { find } = require("../models/Celebrity.model");
+const Celebrity = require("../models/Celebrity.model");
+
+const router = require("express").Router();
+
+// all your routes here
+
+router.get('/celebrities/create', (req,res)=> {
+    res.render('celebrities/new-celebrity')
+})
+
+router.post('/celebrities/create', (req,res)=>{
+    console.log(req.body)
+    const {name, occupation, catchPhrase} = req.body
+    Celebrity.create({name:name,occupation:occupation,catchPhrase:catchPhrase})
+    .then(()=> 
+    res.redirect('/celebrities/celebrities'))
+    .catch((error)=> 
+    res.render('celebrities/new-celebrity')
+    )
+})
+
+router.get('/celebrities',(req,res)=>{
+    Celebrity.find()
+    .then((result)=> {
+        console.log(result)
+        res.render('celebrities/celebrities', {result})
+    })
+    .catch((error)=> {
+        console.log('cought it!', error)
+    })
+})
+
+
+module.exports = router;
