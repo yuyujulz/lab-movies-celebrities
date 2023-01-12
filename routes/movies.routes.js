a2fa75b39f3eeee83569b0ff8bdec78d0de581ca
Sirah Tabarez <sartab2011@gmail.com>
2023-01-12 14:56:25 +0100

    iteration 7 - 9.5

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -39,18 +39,41 @@ router.get('/movies',(req,res)=>{
 })
 
 router.get('/movies/:id', (req,res)=>{
-    MovieModel.findById(req.params)
+    MovieModel.findById(req.params.id)
     .populate('cast')
     .then((movie)=> {
         console.log(movie)
         //Where in my APPLICATION/DIRECTORY is mh hbs that I want to show the user
-        res.render('movies/movie-details', {movie})
+        res.render('movies/movie-details', movie)
     })
     .catch((error)=> {
         console.log('cought it!', error)
     })
 })
 
+router.post('/movies/:movieId/delete', (req, res, next) => {
+    const { movieId } = req.params;
+   
+    MovieModel.findByIdAndDelete(movieId)
+      .then(() => res.redirect('/movies'))
+      .catch(error => next(error));
+  });
 
+
+router.get('/movies/:movieId/edit', (req, res)=>{
+    MovieModel.findById(req.params.movieId)
+    Celebrity.findOne()
+    .then((allDetails)=>{ 
+        res.render('movies/edit-movie', {allDetails})}
+   )
+})
+
+router.post('/movies/:movieId', (req, res, next) => {
+    const { title, genre, plot, cast } = req.body;
+   
+    MovieModel.findByIdAndUpdate(movieId, {title, genre, plot, cast}, {new:true})
+      .then((updateMovie) => res.redirect('/movies/${updateMovie.id}'))
+      .catch(error => next(error));
+  });
 
 module.exports = router;
